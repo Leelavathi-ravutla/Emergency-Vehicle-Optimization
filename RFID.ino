3b25b3ba0c3b87abfb0de995ea74031137a03cb2
Leelavathi-ravutla <104778519+Leelavathi-ravutla@users.noreply.github.com>
2025-03-25 11:08:12 +0530

    RFID.ino

--- RFID.ino
+++ RFID.ino
@@ -1,30 +1,29 @@
-String card1="51007E6DF1B3";
-int led=7;
-int buzzer=8;
+String card1 = "51007E6DF1B3"; // Authorized RFID tag ID
+int led = 7; // LED pin
+int buzzer = 8; // Buzzer pin
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(led,OUTPUT);
-  pinMode(buzzer,OUTPUT);
-  digitalWrite(led,1);
+  pinMode(led, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+  digitalWrite(led, 1); // Initialize LED to unauthorized state
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  while(Serial.available()){
-    String s=Serial.readString();
+  while (Serial.available()) {
+    String s = Serial.readString();
     Serial.println(s);
-    digitalWrite(buzzer,1);
+    digitalWrite(buzzer, 1); // Alert of potential access
     delay(2000);
-    digitalWrite(buzzer,0);
-    if(s==card1) {
-      digitalWrite(led,0);
+    digitalWrite(buzzer, 0);
+    if (s == card1) { // Check if RFID tag ID matches authorized ID
+      digitalWrite(led, 0); // Set LED to authorized state
       Serial.println("Authorized");
+      // Send signal to traffic signal controller to change signals
     } else {
-      digitalWrite(led,1);
+      digitalWrite(led, 1); // Set LED to unauthorized state
       Serial.println("UnAuthorized");
     }
   }
-
 }
+
